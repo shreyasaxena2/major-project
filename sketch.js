ea391bb49a7868b348fdd3565ff2adb962dac343
shreyasaxena2 <shreyasaxena@outlook.com>
2024-11-26 14:15:28 -0600

    setting up variables for the game

--- sketch.js
+++ sketch.js
@@ -5,6 +5,16 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let coingroup;
+let traingroup;
+let path,pathImage;
+let jake,jakeImage;
+let coin,coinImage;
+let train,trainImage;
+let leftBoundary,rightBoundary;
+let sound;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
