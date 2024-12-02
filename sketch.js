81f9161fc1764df6ea4287939e02dba1aaf620fd
shreyasaxena2 <shreyasaxena@outlook.com>
2024-12-02 14:14:26 -0600

    proposal checked

--- sketch.js
+++ sketch.js
@@ -8,10 +8,10 @@
 
 // https://creatorset.com/products/subway-surfers-jake-running
 // https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com
-// %2Fr%2Fnycrail%2Fcomments%2Fw4xnxt%2Fsomething_that_resembles_an_
-// r160143_in_subway%2F&psig=AOvVaw3ETeDZRs9vKP8O6k6Sg0B2&ust=173282445
-// 9999000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCLjZ18Ko_
-// YkDFQAAAAAdAAAAABAE
+// %2Fr%2Fnycrail%2Fcomments%2Fw4xnxt%2Fsomething_that_resembles_an_r160143_in_subway%2F&psig=AOvVaw3ETeDZRs9vKP8O6k6Sg0B2&ust=173282445
+// 9999000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCLjZ18Ko_YkDFQAAAAAdAAAAABAE
+
+// https://editor.p5js.org/gargivanshika/sketches/532yOD6La
 
 let coingroup;
 let traingroup;
